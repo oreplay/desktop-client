cc4b7a3050a1fd63a59e49566240031786e19fee
jarufe <>
2024-02-19 22:19:32 +0100

    Second commit. Model updated as in backend
    (cherry picked from commit 9c7e3a1a6e0cc47d7da5ce761ef74f7280282793)

--- src/eu/oreplay/gui/JPrincipal.java
+++ src/eu/oreplay/gui/JPrincipal.java
@@ -5,6 +5,7 @@
  */
 package eu.oreplay.gui;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import javax.xml.bind.JAXBContext;
 import java.io.*;
 import eu.oreplay.logic.iof.*;
@@ -25,19 +26,31 @@ public class JPrincipal {
         try {
             JAXBContext voContext = null;
             ResultList voResultados = null;
+            StartList voSalidas = null;
             //Primero, test de conversión XML a Clases de un fichero de resultados
+            String vcTipo = "";
             String vcFic = "";
             if (args.length>0) {
-                vcFic = args[0];
+                vcTipo = args[0];
+                vcFic = args[1];
             }
             File voFic = new File(vcFic);
             if (voFic.exists()) {
                 InputStream voIs = new BOMInputStream(new FileInputStream(vcFic));
-                voContext = JAXBContext.newInstance(ResultList.class);
-                voResultados = (ResultList) voContext.createUnmarshaller()
-                        .unmarshal(voIs);
-                if (voResultados!=null) {
-                    System.out.println(voResultados.getEvent().getName());
+                if (vcTipo.equals("Resultados")) {
+                    voContext = JAXBContext.newInstance(ResultList.class);
+                    voResultados = (ResultList) voContext.createUnmarshaller()
+                            .unmarshal(voIs);
+                    if (voResultados!=null) {
+                        System.out.println(voResultados.getEvent().getName());
+                    }
+                } else {
+                    voContext = JAXBContext.newInstance(StartList.class);
+                    voSalidas = (StartList) voContext.createUnmarshaller()
+                            .unmarshal(voIs);
+                    if (voSalidas!=null) {
+                        System.out.println(voSalidas.getEvent().getName());
+                    }
                 }
             } else {
                 System.out.println("El fichero no existe");
@@ -47,8 +60,22 @@ public class JPrincipal {
                 Marshaller mar= voContext.createMarshaller();
                 mar.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
                 mar.marshal(voResultados, new File("./00_PruebaResultados.xml"));
+                //También creo un fichero JSON con Jackson
+                ObjectMapper voMapper = new ObjectMapper();
+                voMapper.writeValue(new File("./00_PruebaResultados.json"), voResultados);
             } else {
                 System.out.println("No tengo resultados para exportar");
+            }
+            //o creación de XML de salidas
+            if (voSalidas!=null) {
+                Marshaller mar= voContext.createMarshaller();
+                mar.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+                mar.marshal(voSalidas, new File("./00_PruebaSalidas.xml"));
+                //También creo un fichero JSON con Jackson
+                ObjectMapper voMapper = new ObjectMapper();
+                voMapper.writeValue(new File("./00_PruebaSalidas.json"), voSalidas);
+            } else {
+                System.out.println("No tengo salidas para exportar");
             }
         }catch(Exception e) {
             e.printStackTrace();
